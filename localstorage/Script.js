eb25dd8a93520296f6dbfc6ae5d55fd97df8b2f6
SriAyu06 <sriayuagustini_2428240139@mhs.mdp.ac.id>
2026-04-06 13:10:14 +0700

    localstorage

---
+++ localstorage/Script.js
@@ -0,0 +1,7 @@
+// simpan local storage
+localStorage.setItem('npm',123)
+// baca data local storage
+console.log(localStorage.getItem('npm'))
+// tampilkan di paragraf id=npm
+document.getElementById('npm').innerHTML = localStorage.getItem('npm')
+
